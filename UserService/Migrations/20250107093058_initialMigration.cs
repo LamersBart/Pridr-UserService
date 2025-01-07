78f3181277e89d03638540ba4c1b8732fea7fe4d
LamersBart <92574075+LamersBart@users.noreply.github.com>
2025-01-07 10:39:36 +0100

    updated migrations

--- UserService/Migrations/20250107093058_initialMigration.cs
+++ UserService/Migrations/20250107093058_initialMigration.cs
@@ -6,32 +6,18 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace UserService.Migrations
 {
     /// <inheritdoc />
-    public partial class initialMigrationNewDB : Migration
+    public partial class initialMigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "User",
-                columns: table => new
-                {
-                    Id = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    KeyCloakId = table.Column<string>(type: "text", nullable: false),
-                    Email = table.Column<string>(type: "text", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_User", x => x.Id);
-                });
-
             migrationBuilder.CreateTable(
                 name: "Profiles",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    UserId = table.Column<int>(type: "integer", nullable: false),
+                    KeyCloakId = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
                     Sexuality = table.Column<int>(type: "integer", nullable: false),
                     LookingFor = table.Column<int>(type: "integer", nullable: false),
                     RelationStatus = table.Column<int>(type: "integer", nullable: false),
@@ -40,25 +26,12 @@ namespace UserService.Migrations
                     Age = table.Column<string>(type: "text", nullable: true),
                     Weight = table.Column<string>(type: "text", nullable: true),
                     Height = table.Column<string>(type: "text", nullable: true),
-                    UserName = table.Column<string>(type: "text", maxLength: 30, nullable: true),
-                    PartnerUserId = table.Column<string>(type: "text", nullable: true)
+                    UserName = table.Column<string>(type: "text", maxLength: 30, nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Profiles", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Profiles_User_UserId",
-                        column: x => x.UserId,
-                        principalTable: "User",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Profiles_UserId",
-                table: "Profiles",
-                column: "UserId",
-                unique: true);
         }
 
         /// <inheritdoc />
@@ -66,9 +39,6 @@ namespace UserService.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Profiles");
-
-            migrationBuilder.DropTable(
-                name: "User");
         }
     }
 }
